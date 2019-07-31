8994f346fb9166b26a24636ffcc832ea10be0638
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-31 15:26:59 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,1 +1,14 @@
+using Documenter
 
+makedocs(
+    # modules = [GUI],
+    sitename = "GUI",
+    pages  = Any[
+        "Overview"                 => "index.md"
+        ]
+    )
+
+deploydocs(
+    repo = "github.com/JeffreySarnoff/GUI.jl.git",
+    target = "build"
+)
