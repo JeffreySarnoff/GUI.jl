f93948b384825bc098a981a3fde2c73999f6ea6c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-09-15 00:48:40 -0400

    Update HTMLPage.jl

--- src/types/HTMLPage.jl
+++ src/types/HTMLPage.jl
@@ -1,11 +1,11 @@
 #=
 
 <!DOCTYPE html?
-<html lang={lang="en"}>
+<html lang="en">
 <head>
-  <meta charset={charset="UTF-8"}>
+  <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>{title=""}</title>
+  <title>""</title>
 </head>
 <body>
 </body>
