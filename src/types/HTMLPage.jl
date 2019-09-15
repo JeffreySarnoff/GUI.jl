6e5f4110ded03d86cfb57d8f626ffe81827a4d80
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-09-15 04:16:01 -0400

    Update HTMLPage.jl

--- src/types/HTMLPage.jl
+++ src/types/HTMLPage.jl
@@ -61,7 +61,7 @@ end
 
 function string(html::HtmlPage)
     io = IOBuffer()
-    write(io, string("<DOCTYPE ", html.doctype,">\n"))
+    write(io, string("<!DOCTYPE ", html.doctype,">\n"))
     write(io, string("<html lang=\"", html.lang, "\">\n"))
     write(io, string(html.head))
     write(io, string(html.body))
