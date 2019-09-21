a288fffbb923e2b94540a405a338af920f42291e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-09-21 02:30:17 -0400

    include `support/linkedlist.jl`

--- src/GUI.jl
+++ src/GUI.jl
@@ -2,7 +2,8 @@ module GUI
 
 using LinkedLists
 
-include("support/macros.jl") # @sym
-include("type/maybe.jl")     # Maybe{Int,Float,Number,String,Symbol}, is{int,float,number,string,symbol}
+include("support/macros.jl")     # @sym
+include("support/linkedlist.jl") # getindex(list, idx::Int)
+include("type/maybe.jl")         # Maybe{Int,Float,Number,String,Symbol}, is{int,float,number,string,symbol}
 
 end # module GUI
