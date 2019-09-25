4e760682be339279ad69789facc8604c6960d978
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-09-24 20:46:08 -0400

    include support/hyperscript.jl

--- src/GUI.jl
+++ src/GUI.jl
@@ -7,4 +7,6 @@ include("support/macros.jl")     # @sym
 include("support/linkedlist.jl") # getindex(list, idx::Int)
 include("type/maybe.jl")         # Maybe{Int,Float,Number,String,Symbol}, is{int,float,number,string,symbol}
 
+include("support/hyperscript.jl") # HTML5 tags
+
 end # module GUI
