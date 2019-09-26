be17688bee2bf8721a61060ec2f531c83cb794aa
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-09-25 21:08:02 -0400

    Update GUI.jl

--- src/GUI.jl
+++ src/GUI.jl
@@ -7,6 +7,7 @@ include("support/macros.jl")     # @sym
 include("support/linkedlist.jl") # getindex(list, idx::Int)
 include("type/maybe.jl")         # Maybe{Int,Float,Number,String,Symbol}, is{int,float,number,string,symbol}
 
-include("support/hyperscript.jl") # HTML5 tags
+include("support/hyperscript_html.jl") # HTML5 tags
+include("support/hyperscript_css.jl")  # CSS3 tags
 
 end # module GUI
