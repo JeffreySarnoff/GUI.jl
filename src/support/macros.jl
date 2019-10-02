3cd2a107c736b490047c28a24db5daac4c6b615c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-10-02 15:43:40 -0400

    Update macros.jl

--- src/support/macros.jl
+++ src/support/macros.jl
@@ -5,3 +5,15 @@
 macro sym(x)
     esc(:(Symbol($x)))
 end
+
+"""
+    @maybe(T) --> Union{Nothing, T}
+    @maybe(T1,T2) --> Union{Nothing, T1, T2}
+"""
+macro maybe(T)
+    :(Union{Nothing, $T})
+end
+
+macro maybe(T1, T2)
+    :(Union{Nothing, $T1, $T2})
+end
