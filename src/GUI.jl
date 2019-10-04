73951a567d9745b0feeaf7587524db4686af6f4d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-10-03 22:01:00 -0400

    using HTTP, Gumbo, Cascadia

--- src/GUI.jl
+++ src/GUI.jl
@@ -1,15 +1,17 @@
 module GUI
 
 using LinkedLists
+using HTTP, Gumbo, Cascadia
 using Hyperscript
 using ReusePatterns
 
-include("support/macros.jl")     # @sym
-include("support/linkedlist.jl") # getindex(list, idx::Int)
-include("type/maybe.jl")         # Maybe{Int,Float,Number,String,Symbol}, is{int,float,number,string,symbol}
+include("support/macros.jl")           # @sym
+include("support/linkedlist.jl")       # getindex(list, idx::Int)
+include("type/maybe.jl")               # Maybe{Int,Float,Number,String,Symbol}, is{int,float,number,string,symbol}
 
-include("support/gumbo.jl")      # typeofnode(x::HTMLNode)
-include("support/cascadia.jl")   #
+include("support/dict.jl")             # getkeys::Vector, getvalues::Vector
+include("support/gumbo.jl")            # typeofnode(x::HTMLNode)
+include("support/cascadia.jl")         #
 
 include("support/hyperscript_html.jl") # HTML5 tags
 include("support/hyperscript_css.jl")  # CSS3 tags
