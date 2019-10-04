da884c1f48e1b4d08d32b6f8bd3a4f507d98f8b4
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-10-03 23:16:07 -0400

    Create abstract.jl

---
+++ src/type/abstract.jl
@@ -0,0 +1,8 @@
+abstract type AbstractGUI end
+
+abstract type AbstractConstrain <: AbstractGUI end
+abstract type AbstractConstruct <: AbstractGUI end
+abstract type AbstractContemper <: AbstractGUI end
+
+abstract type AbstractContent  end
+abstract type AbstractContext  end
