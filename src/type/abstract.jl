133d0cf5abbd30a025c753f3371b2639c7136603
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-10-04 02:21:26 -0400

    Abstract{GUI, Constrain, Construct, Contemper} Abstract{Content, Context}

--- src/type/abstract.jl
+++ src/type/abstract.jl
@@ -1,8 +1,43 @@
+"""
+    AbstractGUI
+
+The root for type abstractions involved in realizing a GUI.
+"""
 abstract type AbstractGUI end
 
+"""
+    AbstractConstrain
+
+The abstraction for types constraining aspects of elements involved in realizing a GUI.
+"""
 abstract type AbstractConstrain <: AbstractGUI end
+
+"""
+    AbstractConstruct
+
+The abstraction for types constructing aspects of elements involved in realizing a GUI.
+"""
 abstract type AbstractConstruct <: AbstractGUI end
+
+"""
+    AbstractContemper
+
+- __contemper__: [_with_][_tempering_], tempering through measured admixture
+
+The abstraction for types contempering aspects of elements involved in realizing a GUI.
+"""
 abstract type AbstractContemper <: AbstractGUI end
 
+"""
+    AbstractContent
+
+The abstraction for types that develop, bring, carry, or hold content involved in realizing a GUI.
+"""
 abstract type AbstractContent  end
+
+"""
+    AbstractContext
+
+The abstraction for types that provide, characterize, organize, or reflect context involved in realizing a GUI.
+"""
 abstract type AbstractContext  end
