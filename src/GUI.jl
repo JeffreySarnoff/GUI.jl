d32bcf25e85e635b41f324b1021dddf03b57ae99
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-10-03 20:01:21 -0400

    include "support/gumbo.jl"

--- src/GUI.jl
+++ src/GUI.jl
@@ -8,6 +8,8 @@ include("support/macros.jl")     # @sym
 include("support/linkedlist.jl") # getindex(list, idx::Int)
 include("type/maybe.jl")         # Maybe{Int,Float,Number,String,Symbol}, is{int,float,number,string,symbol}
 
+include("support/gumbo.jl") # typeofnode(x::HTMLNode)
+
 include("support/hyperscript_html.jl") # HTML5 tags
 include("support/hyperscript_css.jl")  # CSS3 tags
 
