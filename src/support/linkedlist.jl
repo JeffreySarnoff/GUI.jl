bbf3df23763d9fe8915a9facac16cf24e78687da
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-10-08 01:45:28 -0400

    Update linkedlist.jl

--- src/support/linkedlist.jl
+++ src/support/linkedlist.jl
@@ -1,13 +1,11 @@
 # additional functions for use with LinkedLists.jl
 
 function Base.getindex(lst::SLinkedList, idx::Int)
-    !(0 < idx <= length(lst)) && throw(BoundsError("index $idx")
+    !(0 < idx <= length(lst)) && throw(BoundsError("index $idx"))
     return lst[positiontoindex(idx, lst)]
 end
 
 function Base.getindex(lst::LinkedList, idx::Int)
-    !(0 < idx <= length(lst)) && throw(BoundsError("index $idx")
+    !(0 < idx <= length(lst)) && throw(BoundsError("index $idx"))
     return lst[positiontoindex(idx, lst)]
 end
-
-function Base.setin
