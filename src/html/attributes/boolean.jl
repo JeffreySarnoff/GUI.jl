b1135b1a0d9be00657194a5a5e5bf016e7cfa64d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-10-07 21:22:50 -0400

    Create boolean.jl

---
+++ src/html/attributes/boolean.jl
@@ -0,0 +1,35 @@
+const HTML_boolean_attributes = Set([
+    :async
+    :autofocus
+    :autoplay
+    :checked
+    :contenteditable
+    :controls
+    :default
+    :defer
+    :disabled
+    :formNoValidate
+    :frameborder
+    :hidden
+    :ismap
+    :itemscope
+    :loop
+    :multiple
+    :muted
+    :nomodule
+    :novalidate
+    :open
+    :readonly
+    :required
+    :reversed
+    :scoped
+    :selected
+    :typemustmatch
+  ])
+
+HTML_boolean = Dict{Symbol, Tuple{Varargs{Symbol}}}(
+    :disabled => (:button , :fieldset, :input, :optgroup, :option, :select, :textarea),
+    :checked => (:checkbox,)
+  )
+  
+    
