7ac58bfab03d64d07ca9128ace5be245feadd956
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-10-08 09:45:55 -0400

    Update googlefonts.jl

--- test/googlefonts.jl
+++ test/googlefonts.jl
@@ -13,7 +13,7 @@ end
 @testset "Google Font Links" begin
   @test googlefontlink("Roboto") === "<link ref=\"https://fonts.googleapis.com/css?family=Roboto\", rel=\"stylesheet\">"
   @test googlefontlink("IBM Flex Mono") === "<link ref=\"https://fonts.googleapis.com/css?family=IBM+Flex+Mono\", rel=\"stylesheet\">"
-  @test googlefontlink(["Roboto", "IBM Flex Mono"]) === ""<link ref=\"https://fonts.googleapis.com/css?family=Roboto|IBM+Flex+Mono\", rel=\"stylesheet\">"
+  @test googlefontlink(["Roboto", "IBM Flex Mono"]) === "<link ref=\"https://fonts.googleapis.com/css?family=Roboto|IBM+Flex+Mono\", rel=\"stylesheet\">"
   @test googlefontlink("Roboto", weight=[400]) === "<link ref=\"https://fonts.googleapis.com/css?family=Roboto:400\", rel=\"stylesheet\">"
   @test googlefontlink("Roboto", weight=[400,700]) === "<link ref=\"https://fonts.googleapis.com/css?family=Roboto:400,700\", rel=\"stylesheet\">"
   @test googlefontlink("Roboto", weight=[400], style=["","i"]) === "<link ref=\"https://fonts.googleapis.com/css?family=Roboto:400,400i\", rel=\"stylesheet\">"
