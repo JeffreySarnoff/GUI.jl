1aab631d0442a76ecfb1e95707ea5ad98bc06382
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-10-08 08:50:39 -0400

    Update googlefonts.jl

--- src/html/googlefonts.jl
+++ src/html/googlefonts.jl
@@ -1,59 +1,62 @@
 const googlefontstart = "<link ref=\"https://fonts.googleapis.com/css?family="
 const googlefontend   = " rel=\"stylesheet\">"
 
-function googlefontlink(family::String; sizes::Vector{Int}=Int[], styles::Vector{String} = [""], display::String="")
-    str = googlefont(family, sizes=sizes, styles=styles, display=display)
+function googlefontlink(family::String; weight::Vector{Int}=Int[], style::Vector{String} = [""], display::String="")
+    str = googlefont(family, weight=weight, style=style, display=display)
     return string(googlefontstart, str, "\",", googlefontend)
 end
 
-function googlefont(family::String; sizes::Vector{Int}=Int[], styles::Vector{String} = [""], display::String="")
-    xs = googlefont_family(family)
-    str = join(xs, "+")
-    sizes_styles = googlefont_sizes_styles(sizes, styles)
-    if !isempty(sizes_styles)
-        sizes_styles = string(":", sizes_styles)
+function googlefont(family::String; weight::Vector{Int}=Int[], style::Vector{String} = [""], display::String="")
+    str = join(googlefont_family(family), "+")
+    if isempty(weight) && !isempty(style) && !all(isempty.(style))
+        weight = [400]
+    end
+    if isempty(weight)
+        weight_style = ""
+    else
+        weight_style = string(":", googlefont_weight_style(weight, style))
     end
     if !isempty(display)
         display = string("&display=", display)
     end
-    return string(str, sizes_styles, display)
+    return string(str, weight_style, display)
 end
 
 function googlefont_family(family::String)
     return uppercasefirst.(splitstring(family, " "))
 end    
 
-function googlefont_sizes_styles(sizes::Vector{Int}, styles::Vector{String})
+function googlefont_weight_style(weight::Vector{Int}, style::Vector{String})
     strs = String[]
-    for sz in sizes
-        push!(strs, googlefont_size_styles(sz, styles))
+    for w in weight
+        push!(strs, googlefont_weight_style(w, style))
     end
     return join(strs, ",")
 end
 
-function googlefont_size_styles(size::Int, styles::Vector{String})
-    nstyles = length(styles)
-    sizestr = string(size)
+function googlefont_weight_style(weight::Int, style::Vector{String})
+    nstyles = length(style)
+    weightstr = string(weight)
     strs = String[]
     for s in styles
-        push!(strs, string(sizestr, s))
+        push!(strs, string(weightstr, s))
     end
     return join(strs, ",")
 end
 
 function googlefontlink(families::Vector{String};
-                        sizes::Vector{Vector{Int}}=fill(Int[], length(families)), 
+                        weights::Vector{Vector{Int}}=fill(Int[], length(families)), 
                         styles::Vector{Vector{String}}=fill([""], length(families)), display::String="")
-    str = googlefont(families, sizes=sizes, styles=styles, display=display)
+    str = googlefont(families, weights=weights, styles=styles, display=display)
     return string(googlefontstart, str, googlefontend)
 end
        
-function googlefont(families::Vector{String}; sizes, styles, display)
-   (length(families) == length(sizes) == length(styles)) ||
-       throw(ErrorException("inputs ($(length(families)) $(length(sizes)) $(length(styles))) must be of the same length"))
+function googlefont(families::Vector{String}; weights, styles, display)
+   (length(families) == length(weights) == length(styles)) ||
+       throw(ErrorException("inputs ($(length(families)) $(length(weights)) $(length(styles))) must be of the same length"))
    fonts = Vector{Vector{String}}[]
-   for (family,size,style) in zip(families, sizes, styles)
-        push!(fonts, googlefont(family, size, style))
+   for (family,weight,style) in zip(families, weights, styles)
+        push!(fonts, googlefont(family, weight, style))
    end
    return join(fonts, "|")
 end
