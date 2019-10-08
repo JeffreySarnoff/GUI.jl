c06f1b708581c15c571800e46aaf06a54826c876
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-10-08 09:57:38 -0400

    Update googlefonts.jl

--- test/googlefonts.jl
+++ test/googlefonts.jl
@@ -11,11 +11,26 @@ using GUI: googlefont, googlefontlink
 end
 
 @testset "Google Font Links" begin
-  @test googlefontlink("Roboto") === "<link ref=\"https://fonts.googleapis.com/css?family=Roboto\", rel=\"stylesheet\">"
-  @test googlefontlink("IBM Flex Mono") === "<link ref=\"https://fonts.googleapis.com/css?family=IBM+Flex+Mono\", rel=\"stylesheet\">"
-  @test googlefontlink(["Roboto", "IBM Flex Mono"]) === "<link ref=\"https://fonts.googleapis.com/css?family=Roboto|IBM+Flex+Mono\", rel=\"stylesheet\">"
-  @test googlefontlink("Roboto", weight=[400]) === "<link ref=\"https://fonts.googleapis.com/css?family=Roboto:400\", rel=\"stylesheet\">"
-  @test googlefontlink("Roboto", weight=[400,700]) === "<link ref=\"https://fonts.googleapis.com/css?family=Roboto:400,700\", rel=\"stylesheet\">"
-  @test googlefontlink("Roboto", weight=[400], style=["","i"]) === "<link ref=\"https://fonts.googleapis.com/css?family=Roboto:400,400i\", rel=\"stylesheet\">"
-  @test googlefontlink("Roboto", weight=[400,700], style=["","i"]) === "<link ref=\"https://fonts.googleapis.com/css?family=Roboto:400,400i,700,700i\", rel=\"stylesheet\">"
+  @test googlefontlink("Roboto") === "<link ref=\"https://fonts.googleapis.com/css?family=Roboto\", rel=\"stylesheet\">" ||
+        googlefontlink("Roboto") === "<link ref=\"https://fonts.googleapis.com/css?family=Roboto\", rel=\"stylesheet\"/>" ||
+        googlefontlink("Roboto") === "<link ref=\"https://fonts.googleapis.com/css?family=Roboto\", rel=\"stylesheet\"></link>" 
+  @test googlefontlink("IBM Flex Mono") === "<link ref=\"https://fonts.googleapis.com/css?family=IBM+Flex+Mono\", rel=\"stylesheet\">" ||
+        googlefontlink("IBM Flex Mono") === "<link ref=\"https://fonts.googleapis.com/css?family=IBM+Flex+Mono\", rel=\"stylesheet\"/>" ||
+        googlefontlink("IBM Flex Mono") === "<link ref=\"https://fonts.googleapis.com/css?family=IBM+Flex+Mono\", rel=\"stylesheet\"></link>" 
+  @test googlefontlink(["Roboto", "IBM Flex Mono"]) === "<link ref=\"https://fonts.googleapis.com/css?family=Roboto|IBM+Flex+Mono\", rel=\"stylesheet\">" ||
+        googlefontlink(["Roboto", "IBM Flex Mono"]) === "<link ref=\"https://fonts.googleapis.com/css?family=Roboto|IBM+Flex+Mono\", rel=\"stylesheet\"/>" ||
+        googlefontlink(["Roboto", "IBM Flex Mono"]) === "<link ref=\"https://fonts.googleapis.com/css?family=Roboto|IBM+Flex+Mono\", rel=\"stylesheet\"></link>"
+  @test googlefontlink("Roboto", weight=[400]) === "<link ref=\"https://fonts.googleapis.com/css?family=Roboto:400\", rel=\"stylesheet\">" ||
+        googlefontlink("Roboto", weight=[400]) === "<link ref=\"https://fonts.googleapis.com/css?family=Roboto:400\", rel=\"stylesheet\"/>" ||
+        googlefontlink("Roboto", weight=[400]) === "<link ref=\"https://fonts.googleapis.com/css?family=Roboto:400\", rel=\"stylesheet\"></link>"
+  @test googlefontlink("Roboto", weight=[400,700]) === "<link ref=\"https://fonts.googleapis.com/css?family=Roboto:400,700\", rel=\"stylesheet\">" ||
+        googlefontlink("Roboto", weight=[400,700]) === "<link ref=\"https://fonts.googleapis.com/css?family=Roboto:400,700\", rel=\"stylesheet\"/>" ||
+        googlefontlink("Roboto", weight=[400,700]) === "<link ref=\"https://fonts.googleapis.com/css?family=Roboto:400,700\", rel=\"stylesheet\"></link>"
+  @test googlefontlink("Roboto", weight=[400], style=["","i"]) === "<link ref=\"https://fonts.googleapis.com/css?family=Roboto:400,400i\", rel=\"stylesheet\">" ||
+        googlefontlink("Roboto", weight=[400], style=["","i"]) === "<link ref=\"https://fonts.googleapis.com/css?family=Roboto:400,400i\", rel=\"stylesheet\"/>" ||
+        googlefontlink("Roboto", weight=[400], style=["","i"]) === "<link ref=\"https://fonts.googleapis.com/css?family=Roboto:400,400i\", rel=\"stylesheet\"></link>"
+  @test googlefontlink("Roboto", weight=[400,700], style=["","i"]) === "<link ref=\"https://fonts.googleapis.com/css?family=Roboto:400,400i,700,700i\", rel=\"stylesheet\">" ||
+        googlefontlink("Roboto", weight=[400,700], style=["","i"]) === "<link ref=\"https://fonts.googleapis.com/css?family=Roboto:400,400i,700,700i\", rel=\"stylesheet\"/>" ||
+        googlefontlink("Roboto", weight=[400,700], style=["","i"]) === "<link ref=\"https://fonts.googleapis.com/css?family=Roboto:400,400i,700,700i\", rel=\"stylesheet\"></link>"
+
 end
