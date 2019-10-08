f84ea8b2bb9d42570d9f3f00e01ffe85e78e5918
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-10-08 13:31:49 -0400

    Update gumbo.jl

--- src/support/gumbo.jl
+++ src/support/gumbo.jl
@@ -1,3 +1,5 @@
+import Base: string
+
 # subtypes(HTMLNode) == [ HTMLElement, HTMLText, NullNode ]
 
 typeofnode(x::HTMLElement{T}) where {T} = T
@@ -8,3 +10,24 @@ attributes(x::HTMLElement{T}) where {T} = x.attributes
 # attribute_pairs(x::HTMLElement{T}) where {T} = 
 attribute_names(x::HTMLElement{T}) where {T}  = keys(attributes(x))
 attribute_values(x::HTMLElement{T}) where {T} = values(attributes(x))
+
+function string(::Type{HTMLElement{:HTML}}, x, lang="en")
+  doctypestr = "<!DOCTYPE html>"
+  htmlstr = string("<html lang=\"", lang, "\">")
+  headstr = string(::Type{HTMLElement{:head}}, x[1])
+  bodystr = string(::Type{HTMLElement{:body}}, x[2])
+  return join((doctypestr, htmlstr, headstr, "</head>", bodystr, "</body>", "</html>\n"), "\n")
+end
+
+function string(::Type{HTMLElement{:head}}, x)
+    
+end
+
+function string(::Type{HTMLElement{:body}}, x)
+end
+
+
+function Base.string(::Type{HTMLElement{T}}, attributes::Dict) where {T}
+   keys, vals = keys(attributes), values(attributes)
+   string.(join(keys, vals, "="))
+end
