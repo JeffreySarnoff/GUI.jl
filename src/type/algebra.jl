e22937601768f911bbfa53fb8bedc98f467f607d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-10-09 18:54:46 -0400

    Update algebra.jl

--- src/type/algebra.jl
+++ src/type/algebra.jl
@@ -23,68 +23,12 @@
 
 =#
 
-@trait MeroTopoi{X<:AbstractContext, N<:AbstractContent} begin
-    value :: [T] => V  # typeof(value(T)) ==  V
+@trait MeroTopoi{A<:AbstractContext, B<:AbstractContent} begin
+    meld   ::  [A, A] => A
+    whole   :: [B, B] => B
+    pervade :: [A, B] => B
+    inform  :: [A, B] => B
+    suffuse :: [B, A] => A
+    regard  :: [B, A] => A    
 end
 
-@trait Dot{F, V} where {F = vect_infer_helper(V)} begin
-    dot :: [V, V] => F
-    gram_schmidt :: [V, Set{V}] => V
-end
-
-#=
-   Media Types (MIME categories)
-
-   application
-   audio
-   example
-   font
-   image
-   message
-   model
-   multipart
-   text
-   video
-=#
-
-@enum MediaType begin
-   nothing = 0
-   application
-   audio
-   example
-   font
-   image
-   message
-   model
-   multipart
-   text
-   video
-end
-
-struct Content <: AbstractContent
-    uri::MaybeURI
-    mime::MaybeMIME
-    media::MediaType
-    graph::Boolean
-    fromapp::String
-end
-
-const MaybeContent = Union{Nothing, Content}
-iscontent(x::MaybeContent) = !isnothing(x)
-iscontent(x::Content) = true
-iscontent(x) = false
-
-struct Context <: AbastractContext
-    above::MaybeContent
-    below::MaybeContent
-    before::MaybeContent
-    after::MaybeContent
-    backgroundcolor::Color
-    foregroundcolor::Color
-    fontspec::FontSpec
-end
-
-const MaybeContext = Union{Nothing, Context}
-isconteext(x::MaybeContext) = !isnothing(x)
-isccontext(x::Context) = true
-iscontext(x) = false
