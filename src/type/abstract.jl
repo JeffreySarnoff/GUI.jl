e1ea47ee7fb1ad09e8ae63c15c2ef68b6756b81e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-10-09 18:44:59 -0400

    Update abstract.jl

--- src/type/abstract.jl
+++ src/type/abstract.jl
@@ -1,6 +1,6 @@
 #=
     AbstractGUI
-      AbsractContent, AbstractContext
+      AbsractContext, AbstractContent
          AbstractConstrain, AbstractConstruct, AbstractContemper      
 
      AbstractGUI
@@ -22,20 +22,20 @@ The root for type abstractions involved in realizing a GUI.
 abstract type AbstractGUI end
 
 """
-    AbstractContent
-
-The abstraction for types that develop, bring, carry,
-or hold content involved in realizing a GUI.
-"""
-abstract type AbstractContent <: AbstractGUI end
-
-"""
     AbstractContext
 
 The abstraction for types that provide, characterize,
 organize, or reflect context involved in realizing a GUI.
 """
 abstract type AbstractContext  <: AbstractGUI end
+
+"""
+    AbstractContent
+
+The abstraction for types that develop, bring, carry,
+or hold content involved in realizing a GUI.
+"""
+abstract type AbstractContent <: AbstractGUI end
 
         
 """
@@ -47,20 +47,20 @@ involved in realizing a GUI.
 abstract type AbstractConstrain <: AbstractGUI end
 
 """
-    AbstractConstrainContent
-
-The abstraction for types constraining aspects of elements
-involved in realizing the Content of a GUI.
-"""
-abstract type AbstractConstrainContent <: AbstractConstrain end
-
-"""
     AbstractConstrainContext
 
 The abstraction for types constraining aspects of elements
 involved in realizing the Context of a GUI.
 """
 abstract type AbstractConstrainContext <: AbstractConstrain end
+
+"""
+    AbstractConstrainContent
+
+The abstraction for types constraining aspects of elements
+involved in realizing the Content of a GUI.
+"""
+abstract type AbstractConstrainContent <: AbstractConstrain end
 
 
 """
@@ -72,14 +72,6 @@ involved in realizing a GUI.
 abstract type AbstractConstruct <: AbstractGUI end
 
 """
-    AbstractConstructContent
-
-The abstraction for types constructing aspects of elements
-involved in realizing the Content of a GUI.
-"""
-abstract type AbstractConstructContent <: AbstractConstruct end
-
-"""
     AbstractConstructContext
 
 The abstraction for types constructing aspects of elements
@@ -87,6 +79,13 @@ involved in realizing the Context of a GUI.
 """
 abstract type AbstractConstructContext <: AbstractConstruct end
 
+"""
+    AbstractConstructContent
+
+The abstraction for types constructing aspects of elements
+involved in realizing the Content of a GUI.
+"""
+abstract type AbstractConstructContent <: AbstractConstruct end
 
 
 """
@@ -102,17 +101,17 @@ _contemper_ "to gain pliable strength through tempering"
 abstract type AbstractContemper <: AbstractGUI end
 
 """
-    AbstractContemperContent
-
-The abstraction for types constructing aspects of elements
-involved in realizing the Content of a GUI.
-"""
-abstract type AbstractContemperContent <: AbstractContemper end
-
-"""
     AbstractContemperContext
 
 The abstraction for types constructing aspects of elements
 involved in realizing the Context of a GUI.
 """
 abstract type AbstractContemperContext <: AbstractContemper end
+
+"""
+    AbstractContemperContent
+
+The abstraction for types constructing aspects of elements
+involved in realizing the Content of a GUI.
+"""
+abstract type AbstractContemperContent <: AbstractContemper end
