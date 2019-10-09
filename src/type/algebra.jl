112cba74023accf6f42bc7ae8c56ba9e09b9c658
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-10-09 06:18:41 -0400

    Update algebra.jl

--- src/type/algebra.jl
+++ src/type/algebra.jl
@@ -23,3 +23,60 @@
 
 
 =3
+
+#=
+   Media Types (MIME categories)
+
+   application
+   audio
+   example
+   font
+   image
+   message
+   model
+   multipart
+   text
+   video
+=#
+
+@enum MediaType begin
+   nothing = 0
+   application
+   audio
+   example
+   font
+   image
+   message
+   model
+   multipart
+   text
+   video
+end
+
+struct Content <: AbstractContent
+    uri::MaybeURI
+    mime::MaybeMIME
+    media::MediaType
+    graph::Boolean
+    fromapp::String
+end
+
+const MaybeContent = Union{Nothing, Content}
+iscontent(x::MaybeContent) = !isnothing(x)
+iscontent(x::Content) = true
+iscontent(x) = false
+
+struct Context <: AbastractContext
+    above::MaybeContent
+    below::MaybeContent
+    before::MaybeContent
+    after::MaybeContent
+    backgroundcolor::Color
+    foregroundcolor::Color
+    fontspec::FontSpec
+end
+
+const MaybeContext = Union{Nothing, Context}
+isconteext(x::MaybeContext) = !isnothing(x)
+isccontext(x::Context) = true
+iscontext(x) = false
