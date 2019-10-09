a23e08de7c33fe703d585db7fb44a519ca427c9a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-10-09 04:05:16 -0400

    Update algebra.jl

--- src/type/algebra.jl
+++ src/type/algebra.jl
@@ -1,1 +1,25 @@
+#=
+    AbstractGUI
+      AbsractContent, AbstractContext
+         AbstractConstrain, AbstractConstruct, AbstractContemper      
+     AbstractGUI
+       AbstractContent
+          AbstractConstrainContent
+          AbstractConstructContent
+          AbstractContemperContent
+       AbstractContext
+          AbstractConstrainContext
+          AbstractConstructContext
+          AbstractContemperContext
+=#
 
+#=
+    An Algebra of Content and Context
+
+    Jeffrey Sarnoff 2019-Oct-09
+
+    Context pervades  Content
+    Content mapsfrom  Context
+
+
+=3
