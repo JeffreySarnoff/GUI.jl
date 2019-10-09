cb3815f7ff731abc2f8ba4869e105c117412eb12
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-10-09 05:38:09 -0400

    MaybeURI, MaybeMIME

--- src/type/maybe.jl
+++ src/type/maybe.jl
@@ -3,12 +3,16 @@ const MaybeString  = Union{Nothing, String}
 const MaybeNumber  = Union{Nothing, Int, Float64}
 const MaybeInteger = Union{Nothing, Int32, Int64}
 const MaybeFloat   = Union{Nothing, Float32, Float64}
+const MaybeURI     = Union{Nothing, URI}
+const MaybeMIME    = Union{Nothing, MIME}
 
 isinteger(x::MaybeInteger) = !isnothing(x)
 isfloat(x::MaybeFloat)     = !isnothing(x)
 issymbol(x::MaybeSymbol)   = !isnothing(x)
 isstring(x::MaybeString)   = !isnothing(x)
 isnumber(x::MaybeNumber)   = !isnothing(x)
+isuri(x::MaybeURI)         = !isnothing(x)
+ismime(x::MaybeMime)       = !isnothing(x)
 
 isinteger(x::Int32)  = true
 isinteger(x::Int64)  = true
@@ -18,10 +22,14 @@ issymbol(x::Symbol)  = true
 isstring(x::String)  = true
 isnumber(x::Int)     = true
 isnumber(x::Float64) = true
+isuri(x::URI)        = true
+ismime(x::MIME)      = true
 
 isinteger(x) = false
-isfloat(x)  = false
-issymbol(x) = false
-isstring(x) = false
-isnumber(x) = false
+isfloat(x)   = false
+issymbol(x)  = false
+isstring(x)  = false
+isnumber(x)  = false
+isuri(x)     = false
+ismime(x)    = false
 
