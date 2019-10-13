33f52d476b1573fcba43bc6f46eae731b191bebd
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-10-13 01:12:55 -0400

    Update misc.jl

--- src/support/misc.jl
+++ src/support/misc.jl
@@ -1,7 +1,2 @@
 Base.first(x::Nothing) = nothing
 Base.last(x::Nothing)  = nothing
-Base.first(x::Missing) = missing
-Base.last(x::Missing)  = missing
-
-Base.isless(x::T, y::Nothing) where {T} = false
-Base.isless(x::Nothing, y::T) where {T} = true
