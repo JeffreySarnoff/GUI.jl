cc7a69ba44a831c5ff4d41b77146953d15acd384
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-10-24 02:08:50 -0400

    Update algebra.jl

--- src/type/algebra.jl
+++ src/type/algebra.jl
@@ -79,3 +79,36 @@ function hyperelem(x::HtmlElement{N}) where {N}
         x.hyperelem()
     end
 end
+
+#=
+julia> asection = HtmlElement(:section, ("asection",))
+HtmlElement{1}(:section, GUI.section_, ("asection",), nothing)
+
+julia> adiv=HtmlElement(:div, "adiv")
+HtmlElement{1}(:div, GUI.div_, ("adiv",), nothing)
+
+julia> alink=HtmlElement(:a, ("alink"))
+HtmlElement{1}(:a, GUI.a_, ("alink",), nothing)
+
+julia> hyperelem(asection)
+<section class="asection"></section>
+
+julia> hyperelem(adiv)
+<div class="adiv"></div>
+
+julia> hyperelem(alink)
+<a class="alink"></a>
+
+julia> hyperelem(adiv)(hyperelem(alink))
+<div class="adiv"><a class="alink"></a></div>
+
+julia> hyperelem(adiv)(hyperelem(alink)("url"))
+<div class="adiv"><a class="alink">url</a></div>
+
+julia> hyperelem(adiv),hyperelem(alink)("url")
+(<div class="adiv"></div>, <a class="alink">url</a>)
+
+julia> hyperelem(asection)(hyperelem(adiv),hyperelem(alink)("url"))
+<section class="asection"><div class="adiv"></div><a class="alink">url</a></section>
+
+=#
