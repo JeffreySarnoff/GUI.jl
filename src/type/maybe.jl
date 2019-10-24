fe8c525f27a362869698431cc5e56fef0ede6577
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-10-24 00:12:49 -0400

    Update maybe.jl

--- src/type/maybe.jl
+++ src/type/maybe.jl
@@ -1,10 +1,14 @@
-const MaybeSymbol  = Union{Nothing, Symbol}
+const MaybeSymbol  = Union{Nothing, Symbol
 const MaybeString  = Union{Nothing, String}
 const MaybeNumber  = Union{Nothing, Int, Float64}
 const MaybeInteger = Union{Nothing, Int32, Int64}
 const MaybeFloat   = Union{Nothing, Float32, Float64}
 const MaybeURI     = Union{Nothing, URI}
 const MaybeMIME    = Union{Nothing, MIME}
+const MaybeTuple   = Union{Nothing, Tuple}
+const MaybeNTuple{N,T} = Union{Nothing, NTuple{N,T}}
+const MaybeNamedTuple = Union{Nothing, NamedTuple}
+
 
 isinteger(x::MaybeInteger) = !isnothing(x)
 isfloat(x::MaybeFloat)     = !isnothing(x)
@@ -13,6 +17,9 @@ isstring(x::MaybeString)   = !isnothing(x)
 isnumber(x::MaybeNumber)   = !isnothing(x)
 isuri(x::MaybeURI)         = !isnothing(x)
 ismime(x::MaybeMIME)       = !isnothing(x)
+istuple(x::MaybeTuple)     = !isnothing(x)
+isntuple(x::MaybeNTuple{N,T}) where {N,T} = !isnothing(x)
+isnamedtuple(x::MaybeNamedTuple) = !isnothing(x)
 
 isinteger(x::Int32)  = true
 isinteger(x::Int64)  = true
@@ -24,6 +31,9 @@ isnumber(x::Int)     = true
 isnumber(x::Float64) = true
 isuri(x::URI)        = true
 ismime(x::MIME)      = true
+istuple(x::Tuple)    = true
+isntuple(x:NTuple{N,T}) where {N,T} = true
+isnamedtuple(x::NamedTuple) = true
 
 isinteger(x) = false
 isfloat(x)   = false
@@ -32,4 +42,7 @@ isstring(x)  = false
 isnumber(x)  = false
 isuri(x)     = false
 ismime(x)    = false
+istuple(x)   = false
+isntuple(x)  = false
+isnamedtuple(x) = false
 
