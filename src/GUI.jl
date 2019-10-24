41bb1e0e514a1976e28ecc08115afb57636b4e7f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-10-24 03:10:04 -0400

    Update GUI.jl

--- src/GUI.jl
+++ src/GUI.jl
@@ -1,12 +1,13 @@
 module GUI
+include("type/algebra.jl")             #
 
 using DataStructures, LinkedLists, StructArrays, NamedTupleTools, AbstractTrees
 using CanonicalTraits
-using HTTP, HTTP.URIs
 using Gumbo, Cascadia, EzXML
 using HAML, Mustache, Hyperscript
 using ReusePatterns
 
+using HTTP, HTTP.URIs
 include("type/abstract.jl")            #
 
 include("support/macros.jl")           # @sym
@@ -20,7 +21,8 @@ include("support/dict.jl")             # getkeys::Vector, getvalues::Vector
 include("type/prototype.jl")           # specific NamedTuple prototypes
 
 include("html/hyperscript_html.jl")    # HTML5 tags
-#include("css/hyperscript_css.jl")  # CSS3 tags
+#include("css/hyperscript_css.jl")     # CSS3 tags
+include("type/algebra.jl")             # Content, Context, HtmlElement
 
 include("support/ezxml.jl")            # prettyxml
 include("support/gumbo.jl")            # typeofnode(x::HTMLNode)
