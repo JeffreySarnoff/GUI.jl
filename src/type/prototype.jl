9dd5afd603e0d60701c0ce331e3a615274094ae9
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-10-24 21:53:22 -0400

    Update prototype.jl

--- src/type/prototype.jl
+++ src/type/prototype.jl
@@ -10,3 +10,47 @@
 #=
     using the machinery of Hyperscript supporting tag elaboration
 =#
+
+#=
+   sorts of Content
+=#
+
+@enum AkoContent akoNothing=1 akoText akoImage akoForm
+
+const akoContent = Dict(Nothing=>akoNothing, :text=>akoText, :img=>akoImage, :image=>akoImage, :form=>akoForm)
+AkoContent(x::Symbol) = get(akoContent, x, nothing)
+
+mutable struct Content{T} <: AbstractContent
+    ako::AkoContent
+    value::T
+    
+    function Content(value::T) where {T}
+        ako = AkoContent(T)
+        return new{T}(ako, value)
+    end    
+end
+
+const MaybeContent{T} = Union{Nothing, Content{T}}
+iscontent(x::MaybeContent{T}) where {T} = !isnothing(x)
+iscontent(x::Content{T}) where {T} = true
+iscontent(x) = false
+
+#=
+   sorts of Context
+=#
+
+@enum AkoContext akoNothing=1 akoTable akoList akoForm
+
+const akoContext = Dict(Nothing=>akoNothing, :table=>akoTable, :ol=>akoList, :ul=>akoList, :form=>akoForm)
+AkoContext(x::Symbol) = get(akoContext, x, nothing)
+        
+
+mutable struct Context{T} <: AbstractContext
+    ako::AkoContext
+    value::T
+end
+
+const MaybeContext{T} = Union{Nothing, Context{T}}
+iscontext(x::MaybeContext{T}) where {T} = !isnothing(x)
+iscontext(x::Context{T}) where {T} = true
+iscontext(x) = false
