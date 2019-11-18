7131a34595ad8695f629ada976b6c819083ac0b2
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-11-18 10:51:35 -0500

    Update misc.jl

--- src/support/misc.jl
+++ src/support/misc.jl
@@ -1,2 +1,9 @@
+# extend `nothing` to passthrough `first`,`last`, and allied
+Base.iterate(x::Nothing) = nothing
+Base.firstindex(x::Nothing) = nothing
+Base.lastindex(x::Nothing) = nothing
+Base.getindex(collection::Nothing, key::Any) = nothing
+Base.getindex(collection::Any, key::Nothing) = nothing
 Base.first(x::Nothing) = nothing
-Base.last(x::Nothing)  = nothing
+Base.last(x::Nothing) = nothing
+
